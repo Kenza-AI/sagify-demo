3aad66ac6c3626fba285786c807fb0d0b894818b
Joakim Lazakis <ilazakis@gmail.com>
2020-02-25 02:28:27 +0000

    import

--- src/sagify_base/prediction/wsgi.py
+++ src/sagify_base/prediction/wsgi.py
@@ -1,4 +1,4 @@
-from . import predictor as myapp
+from sagify_base.prediction.predictor  import predictor as myapp
 
 # This is just a simple wrapper for gunicorn to find your app.
 # If you want to change the algorithm file, simply change "predictor" above to the
